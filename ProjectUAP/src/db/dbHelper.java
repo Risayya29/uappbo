fdd40008a16f4028171a343b79fb40ded6fcac36
Risayya29 <120788917+Risayya29@users.noreply.github.com>
2022-12-18 23:24:03 +0700

    Add files via upload

---
+++ ProjectUAP/src/db/dbHelper.java
@@ -0,0 +1,8 @@
+package db;
+
+public class dbHelper {
+    private String USERNAME = "root"; 
+    private final String PASSWORD = ""; 
+    private final String DB = "uappbo"; 
+    private final String MYCONN = "jdbc:mysql://localhost/"+DB; 
+}
