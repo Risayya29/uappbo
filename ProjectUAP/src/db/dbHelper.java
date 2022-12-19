8c45278fb67565fec74c9d71b714db41585633e2
Risayya29 <120788917+Risayya29@users.noreply.github.com>
2022-12-20 01:24:51 +0700

    Delete ProjectUAP directory

--- ProjectUAP/src/db/dbHelper.java
+++
@@ -1,8 +0,0 @@
-package db;
-
-public class dbHelper {
-    private String USERNAME = "root"; 
-    private final String PASSWORD = ""; 
-    private final String DB = "uappbo"; 
-    private final String MYCONN = "jdbc:mysql://localhost/"+DB; 
-}
